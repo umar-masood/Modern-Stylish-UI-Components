9aceefdb857c54f4d3c8961d4f1e216a4d5549c4
Umar Masood <umarmasood8546@gmail.com>
2025-08-04 16:50:01 +0500

    Update RoundedBox.cpp

--- RoundedBox.cpp
+++ RoundedBox.cpp
@@ -43,16 +43,6 @@ void RoundedBox::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
 
-    // Rounded mask
-    QBitmap bitmap(width(), height());
-    bitmap.fill(Qt::color0);
-    QPainter maskPainter(&bitmap);
-    maskPainter.setRenderHints(QPainter::Antialiasing);
-    QPainterPath maskPath;
-    maskPath.addRoundedRect(rect(), 6, 6);
-    maskPainter.fillPath(maskPath, Qt::color1);
-    setMask(bitmap);
-
     // Colors
     QColor BG = isDarkMode ? QColor("#1F1F1F") : QColor("#FFFFFF");
     QColor BR = isDarkMode ? QColor("#4D4D4D") : QColor("#CCCCCC");
