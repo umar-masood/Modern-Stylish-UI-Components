25562a15ed54a7c929212bee292a349660ac92ea
Umar Masood <umarmasood8546@gmail.com>
2025-12-25 04:26:55 +0500

    Refactor ToolTip.h to use #pragma once & removed repeated header files for better performance

--- ToolTip.h
+++ ToolTip.h
@@ -1,20 +1,27 @@
-#ifndef TOOLTIP_H
-#define TOOLTIP_H
-
-#include <QtWidgets>
+#pragma once
 #include "RoundedBox.h"
 #include "SmoothOpacity.h"
 
+#include <QObject>
+#include <QPropertyAnimation>
+#include <QTimer>
+#include <QEvent>
+#include <QScreen>
+#include <QApplication>
+#include <algorithm>
+
 class ToolTip : public QObject {
   Q_OBJECT
+  
 public:
-  static void setToolTip(QWidget *widget, const QString &text, bool isDarkMode);
+  explicit ToolTip(QWidget *target = nullptr, const QString &text = "", bool isDarkMode = false);
+  void setText(const QString &text);
+  void setDarkMode(bool isDarkMode);
+  void setTargetWidget(QWidget *target);
+  void hide();
 
 private:
-  explicit ToolTip(QWidget *target, const QString &text, bool isDarkMode);
-
   bool eventFilter(QObject *obj, QEvent *event) override;
-
   void position(QWidget *target);
   void showToolTip();
   void fadeInAnimation();
@@ -25,6 +32,13 @@ private:
   RoundedBox *tooltipWidget = nullptr;
   bool isHovering = false;
   QPropertyAnimation *animation = nullptr;
-};
 
-#endif 
+signals:
+  void textEntered(const QString &text);
+  void themeModeChanged(bool enable);
+
+private slots:
+  void onTextEntered(const QString &text);
+  void onThemeModeChanged(bool enable);
+  void onTimeout();
+};
