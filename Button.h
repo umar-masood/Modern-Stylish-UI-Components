93f901bb9f96f636a337468340737940b1ae0e87
Umar Masood <umarmasood8546@gmail.com>
2025-12-24 07:34:11 +0500

    Refactor: Added Support of Loader Button for Modern Look

--- Button.h
+++ Button.h
@@ -1,6 +1,28 @@
-#include <QtWidgets>
 #pragma once 
 #include "SmoothShadow.h"
+#include "SpinnerProgress.h"
+
+#include <QPushButton>
+#include <QPropertyAnimation>
+#include <QPainter>
+#include <QPainterPath>
+#include <QPaintEvent>
+#include <QMouseEvent>
+#include <QHideEvent>
+#include <QEnterEvent>
+#include <QGraphicsEffect>
+#include <QGraphicsDropShadowEffect>
+#include <QLinearGradient>
+#include <QPen>
+#include <QBrush>
+#include <QPixmap>
+#include <QFont>
+#include <QEasingCurve>
+#include <QColor>
+#include <QSize>
+#include <QRect>
+#include <QString>
+#include <QWidget>
 
 class Button : public QPushButton
 {
@@ -9,19 +31,13 @@ class Button : public QPushButton
   Q_PROPERTY(QColor endColor READ getEndColor WRITE setEndColor)
   
 public:
-  enum DisplayMode
-  {
-    IconOnly,
-    TextOnly,
-    IconText,
-    TextUnderIcon,
-  };
+  enum DisplayMode { IconOnly, TextOnly, IconText, TextUnderIcon, };
 
   explicit Button(const QString &text, QWidget *parent = nullptr);
   explicit Button(QWidget *parent = nullptr);
 
   void setDisplayMode(DisplayMode mode);
-  void setIconPaths(const QString &icon_light = "", const QString &icon_dark = "");
+  void setIconPaths(const QString &iconLight = "", const QString &iconDark = "");
   void setUnicodeIcon(const QString &unicode, int pointSize);
   void setSize(QSize s);
   void setIconSize(QSize s);
@@ -29,16 +45,16 @@ public:
   void setSecondary(bool value);
   void setShadow(bool value);
   void setHyperLink(bool value);
-  void setHyperLinkColors(const QColor &normalState, const QColor &hoverState);\
+  void setHyperLinkColors(const QColor &normalState, const QColor &hoverState);
   void setFontProperties(const QString &family, int pointSize, bool bold = false, bool italic = false);
   void setGradientColor(bool enable, const QString &hex1, const QString &hex2);
   void setHoverGradientColor(const QString &hex);
   void setStartColor(const QColor &c);
   void setEndColor(const QColor &c);
-  QColor getStartColor() const;
-  QColor getEndColor() const;
+  void setLoaderButton(bool value);
+  void setText(const QString &text);
 
-protected:
+  protected:
   void paintEvent(QPaintEvent *event) override;
   void mousePressEvent(QMouseEvent *event) override;
   void mouseReleaseEvent(QMouseEvent *event) override;
@@ -47,30 +63,40 @@ protected:
   void hideEvent(QHideEvent *event) override;
 
 private:
-  void init();
-
-  // States
+  // Flags
   bool isHover = false;
   bool isPressed = false;
   bool isDarkMode = false;
-  bool secondary = false;
-  bool hasShadow = false;
-  bool customSize = false;
-  bool useUnicodeIcon = false;
-  bool useGradient = false;
-  bool hyperLink = false;
+  bool isSecondary = false;
+  bool isShadowEnabled = false;
+  bool isCustomSize = false;
+  bool isUnicodeIcon = false;
+  bool isGradient = false;
+  bool isHyperLink = false;
   bool isBold = false;
   bool isItalic = false;
-
+  bool isLoaderBtn = false;
+  
   bool isDisabledState() const;
   bool isHoverState() const;
   bool isNormalState() const;
   bool isPressedState() const;
   bool isIconOnly() const;
 
+  void init();
+  void drawBorder(QPainter &painter);
+  void drawBackground(QPainter &painter, const QColor &bgColor);
+  void drawContent(QPainter &painter, const QPixmap &pixmap);
+
+  QColor getStartColor() const;
+  QColor getEndColor() const;
+
+  QColor getBackgroundColor() const;
+  QColor getTextColor() const;
+
   // Icons
-  QString light_icon;
-  QString dark_icon;
+  QString lightIcon;
+  QString darkIcon;
 
   // Icons Size
   QSize IconSize = QSize(20, 20);
@@ -81,7 +107,7 @@ private:
   DisplayMode displayMode = IconText;
 
   // Graphical Effects & Animations
-  SmoothDropShadow *effect;
+  SmoothShadow *effect;
   QColor shadow_color;
   QPropertyAnimation *animate;
 
@@ -96,4 +122,7 @@ private:
   // Font 
   QString fontFamily = "Segoe UI";
   int fontSize = 11;
+
+  // Loader
+  SpinnerProgress *spinner = nullptr;
 };
