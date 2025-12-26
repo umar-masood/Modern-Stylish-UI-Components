a90a38bd2c0dd66ffe0706164aae0bfbbc04da6f
Umar Masood <umarmasood8546@gmail.com>
2025-12-26 07:24:41 +0500

    Refactor SmoothDropShadow constructor and method

--- SmoothShadow.cpp
+++ SmoothShadow.cpp
@@ -1,13 +1,10 @@
 #include "SmoothShadow.h"
 
-SmoothDropShadow::SmoothDropShadow(QWidget *parent)
-    : QGraphicsDropShadowEffect(parent), m_radius(0) {}
+SmoothDropShadow::SmoothDropShadow(QWidget *parent) : QGraphicsDropShadowEffect(parent), m_radius(0) {}
 
 void SmoothDropShadow::set_blur_radius(qreal br) {
   m_radius = br;
   QGraphicsDropShadowEffect::setBlurRadius(br);
 }
 
-qreal SmoothDropShadow::get_blur_radius() const {
-  return QGraphicsDropShadowEffect::blurRadius();
-}
+qreal SmoothDropShadow::get_blur_radius() const { return QGraphicsDropShadowEffect::blurRadius(); }
