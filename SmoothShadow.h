9801085fd149dfe36c7975437ad9bad399215181
Umar Masood <umarmasood8546@gmail.com>
2025-12-26 07:24:12 +0500

    Refactor SmoothShadow.h for improved structure

--- SmoothShadow.h
+++ SmoothShadow.h
@@ -1,19 +1,15 @@
-#ifndef SMOOTHSHADOW_H
-#define SMOOTHSHADOW_H
-
-#include <QtWidgets>
+#include <QGraphicsDropShadowEffect>
+#include <QWidget>
 
 class SmoothDropShadow : public QGraphicsDropShadowEffect {
    Q_OBJECT
    Q_PROPERTY(qreal blurRadius READ get_blur_radius WRITE set_blur_radius)
  
- public:
+   public:
    explicit SmoothDropShadow(QWidget *parent = nullptr);
    void set_blur_radius(qreal br);
    qreal get_blur_radius() const;
  
- private:
+   private:
    qreal m_radius;
 };
-
-#endif
